c3432f5737a9fb4f3bf946f912d56eb901dbdf0c
aviralkapoor <kapooraviral411@gmail.com>
2020-02-01 13:44:15 +0530

    .

--- README.md
+++
@@ -1,3 +0,0 @@
-# eVoting
-eVoting System,
-Run 'npm start' in cmd prompt, and visit localhost:3000 in your browser.
--- js/script.js
+++
@@ -1,7 +0,0 @@
-function addText_conduct(item){
-    var content = document.getElementsByClassName("conduct-content");
-  for (i = 0; i < content.length; i++) {
-    content[i].style.display = "none";
-  }
-    document.getElementById(item).style.display="block";
-}
